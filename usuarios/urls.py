6f3e96b771ba48baafe53e35f2f90ebf7ec3d6b5
Carla <c.nadaligdutto@itecriocuarto.org.ar>
2026-09-15 19:03:08 -0300

    chore: actualizar urls

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import usuarios_list, usuarios_detail
 
 urlpatterns = [
-    path('usuarios/', usuarios_list, name='usuarios-list'),
-    path('usuarios/<int:pk>/', usuarios_detail, name='usuarios_detail_api')
+    path('api/usuarios/', usuarios_list, name='usuarios-list'),
+    path('api/usuarios/<int:pk>/', usuarios_detail, name='usuarios_detail_api')
 ]
